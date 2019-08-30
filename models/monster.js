8c84784182c2187b9893ec8a9b24c989c7e05303
d-veazie <dmanvz@gmail.com>
2019-08-29 21:08:27 -0600

    got monster route done, test files in, home.handlebars file, server running

--- models/monster.js
+++ models/monster.js
@@ -23,7 +23,7 @@ module.exports = function(sequelize, DataTypes) {
         allowNull: false
       }
     })
-  Monster.hasMany(model.Stats, {
+  Monster.hasMany(model.Stat, {
     onDelete: "cascade"
   })
   };
