5dc2794712e4d6bfdc978dfadcbce7ae35bf9190
Joby <janthonywolfe@gmail.com>
2019-09-06 21:05:57 -0600

    stuff

--- public/js/index.js
+++ public/js/index.js
@@ -112,7 +112,6 @@ function handleSignupSubmit (e){
   const password = $("#passbox").val().trim();
   console.log("signup form submitted");
   console.log(username, password);
-<<<<<<< HEAD
   $.post("/api/signup", {username: username, password: password}, function(res){
     console.log(res);
     window.location.href="/create";
@@ -120,13 +119,11 @@ function handleSignupSubmit (e){
 }
 
 
-=======
   $.post("http://localhost:3000/api/signup", {username: username, password: password}, function(res){
     console.log(res);
     window.location.href="/create1";
   })
 }
->>>>>>> master
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
